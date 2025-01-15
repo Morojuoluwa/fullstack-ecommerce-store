8706f10cf92825726c37e6853e40caba662c3ae7
abimbola patrick <abimbolapatrickmj@gmail.com>
2025-01-15 14:31:40 +0100

    added the coupon feature on the backend(get coupon & validateCoupon

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.routes.js"
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.routes.js"
+import couponRoutes from "./routes/coupon.route.js"
 import {connectDB} from "./lib/db.js"
 
 dotenv.config()
@@ -15,9 +16,12 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser())
+
+
 app.use("/api/auth", authRoutes)
 app.use("/api/product",productRoutes)
 app.use("/api/cart",cartRoutes)
+app.use("/api/coupons", couponRoutes)
 
 
 app.listen(PORT, ()=>{
