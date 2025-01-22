df34478f5f12c03b6d6551b45c8a3df517690c4a
abimbola patrick <abimbolapatrickmj@gmail.com>
2025-01-22 22:30:18 +0100

    set up the login authentication

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,16 +2,16 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { LogIn, Mail, Lock, ArrowRight, Loader } from "lucide-react";
-// import { useUserStore } from "../stores/useUserStore";
+import { useUserStore } from "../stores/useUserStore";
 
 const LoginPage = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
-	// const { login, loading } = useUserStore();
+	const { login, loading } = useUserStore();
 
-  const login = true
-  const loading = true
+  // const login = true
+  // const loading = true
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
