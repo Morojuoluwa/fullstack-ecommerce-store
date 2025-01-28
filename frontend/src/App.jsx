05a7e1a502d71f67c09fb64b3c9456880d15146e
abimbola patrick <abimbolapatrickmj@gmail.com>
2025-01-28 13:41:00 +0100

    completed the User interface of the cart page. minor chanees might be added later

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,13 +9,20 @@ import { useUserStore } from './stores/useUserStore'
 import LoadingSpinner from './components/LoadingSpinner'
 import AdminPage from './pages/AdminPage'
 import CategoryPage from './pages/CategoryPage'
+import CartPage from './pages/CartPage'
+import { useCartStore } from './stores/useCartStore'
 
 const App = () => {
 	const { user, checkAuth, checkingAuth } = useUserStore();
-	// const { getCartItems } = useCartStore();
+	const { getCartItems } = useCartStore();
 	useEffect(() => {
 		checkAuth();
 	}, [checkAuth]);
+
+  useEffect(()=>{
+   if(!user) return
+    getCartItems()
+  }, [getCartItems, user])
   if (checkingAuth) return <LoadingSpinner/>;
 
   return (
@@ -33,6 +40,7 @@ const App = () => {
           <Route path='/login' element={!user? <LoginPage/> : <Navigate to='/'/>}/>
           <Route path='/secret-dashboard'element={user?.role === "admin" ? <AdminPage /> : <Navigate to='/login' />}/>
           <Route path='/category/:category' element={<CategoryPage/>}/>
+          <Route path='/cart' element={user? <CartPage/> : <Navigate to='/login'/>}/>
         </Routes>
 
       </div>
