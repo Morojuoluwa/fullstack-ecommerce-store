48fd48c070de987921996d6bd6481c456f75e05b
abimbola patrick <abimbolapatrickmj@gmail.com>
2025-01-29 10:26:14 +0100

    added the purchase cancel page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import AdminPage from './pages/AdminPage'
 import CategoryPage from './pages/CategoryPage'
 import CartPage from './pages/CartPage'
 import { useCartStore } from './stores/useCartStore'
+import PurchaseSuccessPage from './pages/PurchaseSuccessPage'
+import PurchaseCancelPage from './pages/PurchaseCancelPage'
 
 const App = () => {
 	const { user, checkAuth, checkingAuth } = useUserStore();
@@ -41,6 +43,8 @@ const App = () => {
           <Route path='/secret-dashboard'element={user?.role === "admin" ? <AdminPage /> : <Navigate to='/login' />}/>
           <Route path='/category/:category' element={<CategoryPage/>}/>
           <Route path='/cart' element={user? <CartPage/> : <Navigate to='/login'/>}/>
+          <Route path='/purchase-success' element={user? <PurchaseSuccessPage/>: <Navigate to='/login'/>}/>
+          <Route path='/purchase-cancel' element={user? <PurchaseCancelPage/>: <Navigate to='/login'/>}/>
         </Routes>
 
       </div>
